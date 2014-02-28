6d589c982302f9997e89d58b8439c8f454027776
oldjunyi <archon.jk@hotmail.com>
2014-02-28 16:36:15 +0800

    update Articulation

--- module/连通性/Articulation.hpp
+++ module/连通性/Articulation.hpp
@@ -11,9 +11,8 @@ public:
     static const int SIZE = 100005; // 最大结点个数
     vector<PII> keyE;               // keyE为割边集，keyV为割点集
     vector<int> keyV,cc[SIZE];      // cc[p]表示结点p在哪些双连通分量中
-    int cnt;  // 遇到奇怪的编译错误时，将上面的cc[SIZE]改成vector形式
+    int cnt;  // 对于旧版编译器，将上面cc[SIZE]改成vector的形式
     // 传入结点个数n及各结点的出边e[]，返回双连通分量的个数cnt
-    // 限制条件：要保证是无向图，且没有重边
     int gao(int n, const vector<int> e[]){
         memset(tag,use=0,sizeof(tag));
         memset(low,cnt=0,sizeof(low));
@@ -25,7 +24,7 @@ public:
 private:
     int tag[SIZE],low[SIZE],dot[SIZE],use;
     void dfs(int x, int dep, const vector<int> e[]){
-        int pt=dep>1;
+        int src=0,out=1<dep;
         dot[use++]=x;
         tag[x]=low[x]=dep;
         for(size_t i=0;i<e[x].size();i++){
@@ -35,12 +34,12 @@ private:
                 low[x]=min(low[x],low[y]);
                 if(low[y]> tag[x]) keyE.push_back(PII(x,y));
                 if(low[y]>=tag[x]){
-                    if(++pt==2) keyV.push_back(x);
+                    if(++out==2) keyV.push_back(x);
                     while(dot[--use]!=y) cc[dot[use]].push_back(cnt);
                     cc[x].push_back(cnt);
                     cc[y].push_back(cnt++);
                 }
-            }else if(tag[y]!=tag[x]-1){
+            }else if(tag[y]!=tag[x]-1 || src++){
                 low[x]=min(low[x],tag[y]);
             }
         }
